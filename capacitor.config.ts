dd10811fe1539eda5e070abbebc6f296ce1fe269
Kronos <44536800+Kronos1914@users.noreply.github.com>
2025-06-09 11:58:59 -0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kronos.webapp',
+  appName: 'KronosWebApp',
+  webDir: '.',
+  bundledWebRuntime: false
+};
+
+export default config;
